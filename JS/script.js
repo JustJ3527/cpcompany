4b7de6dee9b5342d4b716c76bf96f79bade0ee03
JustJ3527 <antoine.jules04@gmail.com>
2023-03-12 14:29:02 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,20 +4,32 @@ $(document).ready(function(){
             document.getElementById("img-1").style.filter = "grayscale(0)";
             document.getElementById("img-2").style.filter = "grayscale(1)";
             document.getElementById("img-3").style.filter = "grayscale(1)";
+            document.getElementById("img-1").style.scale = "1.1";
+            document.getElementById("img-2").style.scale = "none";
+            document.getElementById("img-3").style.scale = "none";
         }else{
             if($("#img-2:hover").length != 0){
             document.getElementById("img-1").style.filter = "grayscale(1)";
             document.getElementById("img-2").style.filter = "grayscale(0)";
             document.getElementById("img-3").style.filter = "grayscale(1)";
+            document.getElementById("img-1").style.scale = "none";
+            document.getElementById("img-2").style.scale = "1.1";
+            document.getElementById("img-3").style.scale = "none";
             }else{
                 if($("#img-3:hover").length != 0){
                 document.getElementById("img-1").style.filter = "grayscale(1)";
                 document.getElementById("img-2").style.filter = "grayscale(1)";
                 document.getElementById("img-3").style.filter = "grayscale(0)";
+                document.getElementById("img-1").style.scale = "none";
+                document.getElementById("img-2").style.scale = "none";
+                document.getElementById("img-3").style.scale = "1.1";
                 }else{
                     document.getElementById("img-1").style.filter = "grayscale(0)";
                     document.getElementById("img-2").style.filter = "grayscale(0)";
                     document.getElementById("img-3").style.filter = "grayscale(0)";
+                    document.getElementById("img-1").style.scale = "none";
+                    document.getElementById("img-2").style.scale = "none";
+                    document.getElementById("img-3").style.scale = "none";
                 }
             }
         }
