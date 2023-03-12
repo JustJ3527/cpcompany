a10e8a5cd86093075a4cd9ac8cf87c5584e17e1f
JustJ3527 <antoine.jules04@gmail.com>
2023-03-12 20:59:57 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -35,3 +35,30 @@ $(document).ready(function(){
         }
     });
 });
+
+function lightMode() {
+    document.documentElement.style.setProperty('--darkColor', 'rgb(255, 255, 255)');
+    document.documentElement.style.setProperty('--lightColor', 'rgb(11, 11, 11)');
+    document.documentElement.style.setProperty('--lightSecondaryColor', 'rgb(117, 171, 212)');
+}
+
+function darkMode() {
+    document.documentElement.style.setProperty('--lightColor', 'rgb(255, 255, 255)');
+    document.documentElement.style.setProperty('--darkColor', 'rgb(11, 11, 11)');
+    document.documentElement.style.setProperty('--lightSecondaryColor', 'rgb(117, 171, 212)');
+}
+
+var mode = 0; //dark
+var logo = document.getElementById("logo");
+
+function changeMode() {
+    if(mode == 0){
+        lightMode();
+        document.getElementById("logo").src="./assets/logo_black.png";
+        mode = 1;
+    }else if(mode == 1){
+        darkMode();
+        document.getElementById("logo").src="./assets/logo.png";
+        mode = 0;
+    }
+}
